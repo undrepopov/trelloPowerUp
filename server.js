b3dda13d6392c60aba0e691c94ad7582a6881b1b
Andrey Popov <andrey.popov@herzum.com>
2017-10-11 13:58:52 -0500

    moved some files around

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("*", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + '/index.html');
 });
 
 // listen for requests :)
